92f84d5796a8973921c70fec841ff4ae55e8dcc2
Jeremiah <jermcdon18@gmail.com>
2025-09-16 10:42:58 -0500

    set OnClickListeners for reset and save.

--- app/src/main/java/com/example/pokemontracker/MainActivity.java
+++ app/src/main/java/com/example/pokemontracker/MainActivity.java
@@ -4,11 +4,13 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.text.TextWatcher;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -54,6 +56,22 @@ public class MainActivity extends AppCompatActivity {
         attachUnits();
         setFiltersAndCaps();
         setDefaults();
+
+        // Buttons
+        Button reset = findViewById(R.id.reset);
+        Button save = findViewById(R.id.save);
+
+        reset.setOnClickListener(v -> setDefaults());
+
+        save.setOnClickListener(v -> {
+            clearLabelColors();
+            String error = validateAll();
+            if (error.isEmpty()) {
+                Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
